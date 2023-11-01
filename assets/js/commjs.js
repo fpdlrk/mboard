ea6630daf1a0fd9f70da1f6a23a5bb82706aab62
fpdlrk <61310220+fpdlrk@users.noreply.github.com>
2023-11-01 20:55:01 +0900

    커밋

--- assets/js/commjs.js
+++ assets/js/commjs.js
@@ -1,4 +1,3 @@
-
 const commjs = {
 	// workspace의 라벨로 쓰기위해 앞 첫글자를 가져온다.
 	nameFirstAt : function(firstAt){
@@ -73,58 +72,3 @@ const toggleEvtLoop = function(items){
 toggleEvtLoop(toggleItem);
 fnLnbItemShowHide(lnbItem, 0);
 
-//로딩 스크립트
-var loadingHtml = '';
-loadingHtml += '<div class="l-wrapper loading_wrap">';
-loadingHtml += '<svg viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">';
-loadingHtml += '<symbol id="s--circle">';
-loadingHtml += '<circle r="10" cx="20" cy="20"></circle>';
-loadingHtml += '</symbol>';
-loadingHtml += '<g class="g-circles g-circles--v1">';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '<g class="g--circle">';
-loadingHtml += '<use xlink:href="#s--circle" class="u--circle"/>';
-loadingHtml += '</g>';
-loadingHtml += '</g>';
-loadingHtml += '</svg>';
-loadingHtml += '</div>';
-
-function fn_loading() {
-  $("body").append(loadingHtml);
-}
-
-function fn_loadingEnd() {
-  $(".loading_wrap").remove();
-}
